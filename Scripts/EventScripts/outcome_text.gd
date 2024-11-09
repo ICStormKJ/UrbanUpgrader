fc0221a027a7e95237b08734d1ce050bf59ae10b
Kai Jinnai <kai_jinnai27@hotmail.com>
2024-11-09 17:31:50 -0500

    Folder stuff

---
+++ Scripts/EventScripts/outcome_text.gd
@@ -0,0 +1,5 @@
+extends Button
+
+@export_multiline var outcomeDescription: String
+@export var repValue: int
+@export var moneyValue: int
--- outcome_text.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-@export_multiline var outcomeDescription: String
-@export var repValue: int
-@export var moneyValue: int
